1d34275b192b91734507f0c78d5d4cd2934c1a6c
JJNugent <70417327+JJNugent@users.noreply.github.com>
2022-10-16 22:28:45 -0600

    Typescript Boiler Plate

---
+++ scripts/hello-world.ts
@@ -0,0 +1,11 @@
+//This is a typescript file to demonstrate that our typescript boiler-plate works
+
+interface Greetings {
+    name: string;
+}
+
+let Hello : Greetings = {
+    name: "Hello World!"
+};
+
+console.log(Hello.name);
